a9ab0c2b2c833e05993fe066f4f3611a9502420e
Elijah Wilkes <twilkes@ameronix.com>
2018-02-27 11:37:11 -0500

    Get og-react-swipe to first fully working state

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,50 +1,229 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class Card extends Component {
 	constructor(props) {
 		super(props);
 
-		this.drag = this.drag.bind(this);
+		this.grab = this.grab.bind(this);
+		this.grabTouch = this.grabTouch.bind(this);
+		this.move = this.move.bind(this);
+		this.moveTouch = this.moveTouch.bind(this);
 		this.drop = this.drop.bind(this);
+		this.dropTouch = this.dropTouch.bind(this);
+
+		this.setGrabbedPos = this.setGrabbedPos.bind(this);
+
+		this.state = {
+			start_top: undefined,
+			start_left: undefined,
+			grabbed: false,
+			left_diff: 0,
+			top_diff: 0,
+		};
+	}
+
+	componentDidMount() {
+		if (this.props.visible) this.setStateSize();
+	}
+
+	componentDidUpdate(prevProps) {
+		if (!prevProps.visible && this.props.visible) this.setStateSize();
+	}
+
+	componentWillUnmount() {
+		if (typeof document !== 'undefined') {
+			this.state.elem.removeEventListener('touchstart', this.grabTouch);
+			this.state.elem.removeEventListener('mousedown', this.grab);
+		}
 	}
 
 	render() {
-		let props = {};
+		let card_props = {
+			className: 'og-card',
+		};
 
-		if (this.props.top_bound) {
-			props = {
-				draggable: true,
-				onDragStart: this.drag,
-				//onDrag: this.move,
-				onDragEnd: this.drop,
-				onTouchStart: this.drag,
-				//onTouchMove: this.move,
-				onTouchEnd: this.drop,
-			};
+		if (this.state.grabbed) {
+			card_props.className = 'hidden';
 		}
 
-		if (this.props.className) props.className = this.props.className;
+		let grabbed_props = {
+			className: 'og-card-grabbed',
+			style: {
+				height: this.state.height,
+				width: this.state.width,
+			},
+		};
+
+		if (!this.state.grabbed) grabbed_props.className += ' hidden';
+
+		let container_className = 'og-card-container';
+
+		if (!this.props.visible) container_className += ' hidden';
 
 		return (
-			<div { ...props }>
-				{ this.props.children }
+			<div className={ container_className }>
+				<div { ...card_props } ref={(n) => { this.placeholder = n; }}>
+					{ this.props.children }
+				</div>
+				<div { ...grabbed_props } ref={(n) => { this.grabbed = n; }}>
+					{ this.props.children }
+				</div>
 			</div>
 		);
 	}
 
-	drag({ target }) {
-		console.log('drag');
-		console.log(target);
+	grab(e) {
+		let left_diff = e.x - this.state.start_left;
+		let top_diff = e.y - this.state.start_top;
+
+		this.setState({ 
+			grabbed: true,
+			left_diff,
+			top_diff,
+		}, () => {
+			this.props.showNext();
+			this.setGrabbedPos(e.x, e.y);
+		});
+
+		document.addEventListener('mousemove', this.move);
+		document.addEventListener('mouseup', this.drop);
 	}
 
-	move(e) {
-		console.log('move');
-		//console.log(e.clientX);
+	grabTouch(e) {
+		e.preventDefault();
+
+		let coords = {
+			x: e.touches[0].clientX,
+			y: e.touches[0].clientY,
+		};
+
+		let left_diff = coords.x - this.state.start_left;
+		let top_diff = coords.y - this.state.start_top;
+
+		this.setState({
+			grabbed: true,
+			left_diff,
+			top_diff,
+		}, () => {
+			this.props.showNext();
+			this.setGrabbedPos(coords.x, coords.y);
+		});
+
+		document.addEventListener('touchmove', this.moveTouch);
+		document.addEventListener('touchend', this.dropTouch);
 	}
 
 	drop(e) {
-		console.log('drop');
-		console.log(e.clientX);
+		if (/(-?\d+).*px/.test(this.grabbed.style.left) 
+			&& /(-?\d+).*px/.test(this.grabbed.style.top)) {
+			let x = /(-?\d+).*px/.exec(this.grabbed.style.left)[1];
+			let y = /(-?\d+).*px/.exec(this.grabbed.style.top)[1];
+
+			this.fireDroppedEvents(parseInt(x), parseInt(y));
+		}
+
+		this.setState({ grabbed: false });
+
+		document.removeEventListener('mousemove', this.move);
+		document.removeEventListener('mouseup', this.drop);
+	}
+
+	dropTouch(e) {
+		if (/(-?\d+).*px/.test(this.grabbed.style.left) 
+			&& /(-?\d+).*px/.test(this.grabbed.style.top)) {
+			let x = /(-?\d+).*px/.exec(this.grabbed.style.left)[1];
+			let y = /(-?\d+).*px/.exec(this.grabbed.style.top)[1];
+
+			this.fireDroppedEvents(parseInt(x), parseInt(y));
+		}
+
+		this.setState({ grabbed: false });
+
+		document.removeEventListener('touchmove', this.moveTouch);
+		document.removeEventListener('touchend', this.dropTouch);
+	}
+
+	move(e) {
+		this.setGrabbedPos(e.x, e.y);
+	}
+
+	moveTouch(e) {
+		let x = e.touches[0].clientX;
+		let y = e.touches[0].clientY;
+
+		this.setGrabbedPos(x, y);
+	}
+
+	setGrabbedPos(x, y) {
+		this.grabbed.style.left = 
+			(x - this.state.start_left - this.state.left_diff) + 'px';
+		this.grabbed.style.top = 
+			(y - this.state.start_top - this.state.top_diff) + 'px';
+	}
+
+	fireDroppedEvents(x, y) {
+		let direction = null;
+		let amount = 0;
+
+		let abs = Math.abs;
+
+		if (this.props.onRight && x > this.props.right_bound && x > amount) {
+			direction = 'right';
+			amount = x;
+		} else if (this.props.onLeft && x < this.props.left_bound && abs(x) > amount) {
+			direction = 'left';
+			amount = abs(x);
+		}
+
+		if (this.props.onBottom && y > this.props.bottom_bound && y > amount) {
+			direction = 'bottom'
+			amount = y;
+		} else if (this.props.onTop && y < this.props.top_bound && abs(y) > amount) {
+			direction = 'top';
+			amount = abs(y);
+		}
+
+		switch (direction) {
+			case 'top':
+				this.props.onTop(amount);
+				break;
+			case 'right':
+				this.props.onRight(amount);
+				break;
+			case 'bottom':
+				this.props.onBottom(amount);
+				break;
+			case 'left':
+				this.props.onLeft(amount);
+				break;
+			default:
+				this.props.revert();
+				break;
+		}
+	}
+
+	setStateSize() {
+		let rect = ReactDOM.findDOMNode(this)
+			.getBoundingClientRect();
+
+		let start_top = rect.top;
+		let start_left = rect.left;
+
+		if (typeof document !== 'undefined') {
+			let elem = ReactDOM.findDOMNode(this);
+
+			elem.addEventListener('touchstart', this.grabTouch);
+			elem.addEventListener('mousedown', this.grab);
+
+			this.setState({ 
+				element: elem,
+				width: this.placeholder.clientWidth,
+				height: this.placeholder.clientHeight,
+				start_top,
+				start_left,
+			});
+		}
 	}
 }
 
--- src/components/CardStack/index.js
+++ src/components/CardStack/index.js
@@ -1,52 +1,143 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import { throttle } from '../../utility.js';
+
 class CardStack extends Component {
 	constructor(props) {
 		super(props);
+
+		this.onTop = this.onTop.bind(this);
+		this.onRight = this.onRight.bind(this);
+		this.onBottom = this.onBottom.bind(this);
+		this.onLeft = this.onLeft.bind(this);
+		this.revert = this.revert.bind(this);
+		this.showNext = this.showNext.bind(this);
 
 		this.state = {
 			top_bound: undefined,
 			right_bound: undefined,
 			bottom_bound: undefined,
 			left_bound: undefined,
-			onTop: this.props.onTop || undefined,
-			onRight: this.props.onRight || undefined,
-			onBottom: this.props.onBottom || undefined,
-			onLeft: this.props.onLeft || undefined,
+			onTop: this.props.onTop ? this.onTop : undefined,
+			onRight: this.props.onRight ? this.onRight : undefined,
+			onBottom: this.props.onBottom ? this.onBottom : undefined,
+			onLeft: this.props.onLeft ? this.onLeft : undefined,
+			currently_viewed: this.props.start_index || 0,
 		};
+
+		this.refs = {};
 	}
 	
 	componentDidMount() {
-		let rect = ReactDOM.findDOMNode(this)
-			.getBoundingClientRect();
+		let diff = this.props.diff || 100;
 
-		if (rect) {
-			let diff = this.props.diff || 20;
-
-			this.setState({
-				top_bound: rect.top - diff,
-				right_bound: rect.right + diff,
-				bottom_bound: rect.bottom + diff,
-				left_bound: rect.left - diff,
-			});
-		}
+		this.setState({
+			top_bound: -diff,
+			right_bound: diff,
+			bottom_bound: diff,
+			left_bound: -diff,
+		});
 	}
 
 	render() {
 		let className = 'og-card-stack';
-
 		if (this.props.className) className += ' ' + this.props.className;
 
-		let children = this.props.children.map(child => {
-			return React.cloneElement(child, this.state, null);
-		});
+		let default_child_props = {
+			top_bound: this.state.top_bound,
+			right_bound: this.state.right_bound,
+			bottom_bound: this.state.bottom_bound,
+			left_bound: this.state.left_bound,
+			onTop: this.state.onTop,
+			onRight: this.state.onRight,
+			onBottom: this.state.onBottom,
+			onLeft: this.state.onLeft,
+			revert: this.revert,
+			showNext: this.showNext,
+		};
+
+		if (this.state.styleOnMove) default_child_props.styleOnMove = this.state.styleOnMove;
+
+		let children;
+			
+		if (this.props.children && Array.isArray(this.props.children)) {
+			children = this.props.children.map((child, i) => {
+				let child_props = { 
+					...child.props, 
+					...default_child_props,
+					visible: 
+						this.state.currently_viewed == i 
+						|| this.state.next_visible && this.state.currently_viewed + 1 == i
+						? true : false,
+				};
+
+				return React.cloneElement(child, child_props, child.props.children);
+			});
+		} else if (this.props.children) {
+			let child_props = { 
+				...this.props.children.props, 
+				...default_child_props,
+				visible: 
+					this.state.currently_viewed == 0
+					? true : false,
+			};
+
+			children = React.cloneElement(
+				this.props.children, 
+				child_props, 
+				this.props.children.props.children
+			);
+		}
+
+		let style = { ...this.props.style };
 
 		return (
-			<div className={ className }>
+			<div className={ className } style={ this.props.style }>
 				{ children }
 			</div>
 		);
+	}
+
+	onTop(amount) {
+		this.props.onTop(amount);
+
+		this.incrementView();
+	}
+
+	onRight(amount) {
+		this.props.onRight(amount);
+
+		this.incrementView();
+	}
+
+	onBottom(amount) {
+		this.props.onBottom(amount);
+
+		this.incrementView();
+	}
+
+	onLeft(amount) {
+		this.props.onLeft(amount);
+
+		this.incrementView();
+	}
+
+	revert() {
+		this.setState({ next_visible: false });
+	}
+
+	showNext() {
+		this.setState({ next_visible: true });
+	}
+
+	incrementView() {
+		let currently_viewed = this.state.currently_viewed + 1;
+
+		this.setState({ currently_viewed, next_visible: false }, () => {
+			if (this.props.onRunOut && currently_viewed >= this.props.children.length)
+				this.props.onRunOut();
+		});
 	}
 }
 
